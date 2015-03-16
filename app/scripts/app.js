527f9d25c1117bd78af75e2ae8fa3757116797d8
Anton Kann <a.kann@intrallect.com>
2015-03-16 16:09:24 +0000

    Create angular app using Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lastCloudAppApp
+ * @description
+ * # lastCloudAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lastCloudAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
